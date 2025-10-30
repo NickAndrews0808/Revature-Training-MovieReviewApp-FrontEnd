a09b046ab5caf14a87a83712f90af4d6cb56a461
Haiku Patel <haikupatel211@gmail.com>
2025-10-30 07:20:57 -0500

    Login page HTML

--- movie_review_app/src/pages/Login/Login.jsx
+++ movie_review_app/src/pages/Login/Login.jsx
@@ -3,14 +3,8 @@ import './Login.css';
 
 export default function Login() {
   return (
-    <div className="login-page">
-      <div className="login-container">
-        <div className="welcome-text">
-          <h1>Welcome Back!</h1>
-          <p>Sign in to continue to your account</p>
-        </div>
-        <LoginForm />
-      </div>
+    <div >
+      <LoginForm />
     </div>
   );
 }
