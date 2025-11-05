871b99c62eaab5f739e05ac79783ee4cf823a15b
Leo6648 <jchen6648@gmail.com>
2025-11-05 11:07:12 -0600

    Change base url 2

--- movie_review_app/src/api/endpoints.js
+++ movie_review_app/src/api/endpoints.js
@@ -1,5 +1,5 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
-export const API_BASE_URL = isDevelopment? 'http://trng2309-7.s3-website.us-east-2.amazonaws.com' : 'abc';//replace with actual production URL
+export const API_BASE_URL = 'http://trng2309-7.s3-website.us-east-2.amazonaws.com';//replace with actual production URL
 export const FRONTEND_URL = isDevelopment ? 'http://localhost:5173' : 'abc';
 
 export const API_ENDPOINTS={
