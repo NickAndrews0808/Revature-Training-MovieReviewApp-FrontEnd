d9ef488e4805d007b618a3df84a572cf6a4ab1bd
Haiku Patel <haikupatel211@gmail.com>
2025-11-06 08:02:40 -0600

    url change

--- movie_review_app/src/api/endpoints.js
+++ movie_review_app/src/api/endpoints.js
@@ -1,6 +1,6 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
-// export const API_BASE_URL = 'http://ec2-54-234-94-174.compute-1.amazonaws.com:8087';//replace with actual production URL
-export const API_BASE_URL = 'http://localhost:8087';//replace with actual production URL
+export const API_BASE_URL = 'http://ec2-54-234-94-174.compute-1.amazonaws.com:8087';//replace with actual production URL
+// export const API_BASE_URL = 'http://localhost:8087';//replace with actual production URL
 export const FRONTEND_URL = 'http://localhost:5173';
 
 export const API_ENDPOINTS={
